a5d8773445a88b53886a2631366994ae726251b9
Kezheng Zhang <846009097@qq.com>
2023-05-18 15:06:09 +0800

    topn算子出了点问题

--- src/execution/topn_executor.cpp
+++ src/execution/topn_executor.cpp
@@ -1,6 +1,7 @@
 #include "execution/executors/topn_executor.h"
 #include <queue>
 #include <vector>
+#include "storage/table/tuple.h"
 
 namespace bustub {
 
@@ -9,59 +10,58 @@ TopNExecutor::TopNExecutor(ExecutorContext *exec_ctx, const TopNPlanNode *plan,
     : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
 void TopNExecutor::Init() {
-  child_executor_->Init();
-  std::stack<Tuple>().swap(child_tuples_);
-  auto cmp = [order_bys = plan_->GetOrderBy(), schema = child_executor_->GetOutputSchema()](const Tuple &tuple_a,
-                                                                                            const Tuple &tuple_b) {
-    for (const auto &order_key : order_bys) {
-      switch (order_key.first) {
-        case OrderByType::INVALID:
-        case OrderByType::DEFAULT:
-        case OrderByType::ASC:
-          if (static_cast<bool>(order_key.second->Evaluate(&tuple_a, schema)
-                                    .CompareLessThan(order_key.second->Evaluate(&tuple_b, schema)))) {
-            return true;
-          } else if (static_cast<bool>(order_key.second->Evaluate(&tuple_a, schema)
-                                           .CompareGreaterThan(order_key.second->Evaluate(&tuple_b, schema)))) {
-            return false;
-          }
+    child_executor_->Init();
+    Tuple tuple{};
+    RID rid{};
+    std::stack<Tuple>().swap(child_tuples_);
+    auto cmp=[order_bys=plan_->GetOrderBy(),schema=child_executor_->GetOutputSchema()](const Tuple& tuple_a,const Tuple& tuple_b)->bool{
+      for(const auto &order_by:order_bys){
+        switch(order_by.first){
+          case OrderByType::ASC:
+          case OrderByType::INVALID:
+          case OrderByType::DEFAULT:
+             if (static_cast<bool>(order_by.second->Evaluate(&tuple_a, schema)
+                                        .CompareLessThan(order_by.second->Evaluate(&tuple_b, schema)))) {
+                return true;
+              }
+              if(static_cast<bool>(order_by.second->Evaluate(&tuple_b,schema).CompareGreaterThan(order_by.second->Evaluate(&tuple_b, schema)))){
+                return false;
+              }
           break;
-        case OrderByType::DESC:
-          if (static_cast<bool>(order_key.second->Evaluate(&tuple_a, schema)
-                                    .CompareGreaterThan(order_key.second->Evaluate(&tuple_b, schema)))) {
-            return true;
-          } else if (static_cast<bool>(order_key.second->Evaluate(&tuple_a, schema)
-                                           .CompareLessThan(order_key.second->Evaluate(&tuple_b, schema)))) {
-            return false;
-          }
+          case OrderByType::DESC:
+           if (static_cast<bool>(order_by.second->Evaluate(&tuple_a, schema)
+                                        .CompareGreaterThan(order_by.second->Evaluate(&tuple_b, schema)))) {
+                return true;
+           }
+           if (static_cast<bool>(order_by.second->Evaluate(&tuple_a, schema)
+                                               .CompareLessThan(order_by.second->Evaluate(&tuple_b, schema)))) {
+                return false;
+           }
           break;
+        }
       }
+      return false;
+    };
+    std::priority_queue<Tuple,std::vector<Tuple>,decltype(cmp)> pq(cmp);
+    while(child_executor_->Next(&tuple, &rid)){
+        pq.push(tuple);
+        if (pq.size() > plan_->GetN()) {
+          pq.pop();
+        }
     }
-    return false;
-  };
-  std::priority_queue<Tuple, std::vector<Tuple>, decltype(cmp)> pq(cmp);
-  Tuple tuple{};
-  RID rid{};
-  while (child_executor_->Next(&tuple, &rid)) {
-    pq.push(tuple);
-    if (pq.size() > plan_->GetN()) {
+    while(!pq.empty()){
+      child_tuples_.push(pq.top());
       pq.pop();
     }
-  }
-  while (!pq.empty()) {
-    child_tuples_.push(pq.top());
-    pq.pop();
-  }
 }
 
 auto TopNExecutor::Next(Tuple *tuple, RID *rid) -> bool {
-  if (child_tuples_.empty()) {
+  if(child_tuples_.empty()){
     return false;
   }
-  *tuple = child_tuples_.top();
-  *rid = tuple->GetRid();
+  *tuple=child_tuples_.top();
+  *rid=tuple->GetRid();
   child_tuples_.pop();
   return true;
 }
-
 }  // namespace bustub
--- src/optimizer/sort_limit_as_topn.cpp
+++ src/optimizer/sort_limit_as_topn.cpp
@@ -13,22 +13,13 @@ auto Optimizer::OptimizeSortLimitAsTopN(const AbstractPlanNodeRef &plan) -> Abst
     children.emplace_back(OptimizeSortLimitAsTopN(child));
   }
   auto optimized_plan = plan->CloneWithChildren(std::move(children));
-  if (optimized_plan->GetType() == PlanType::Limit) {
-    const auto &limit_plan = dynamic_cast<const LimitPlanNode &>(*optimized_plan);
-    const auto &limit = limit_plan.GetLimit();
-
-    // Has exactly one child
-    BUSTUB_ENSURE(optimized_plan->children_.size() == 1, "Limit Plan should have exactly one child!");
-    const auto &child_plan = optimized_plan->children_[0];
-
-    if (child_plan->GetType() == PlanType::Sort) {
-      const auto &sort_plan = dynamic_cast<const SortPlanNode &>(*optimized_plan->GetChildAt(0));
-      const auto &order_bys = sort_plan.GetOrderBy();
-      // Has exactly one child
-      BUSTUB_ENSURE(optimized_plan->children_.size() == 1, "Sort with multiple children?? Impossible!");
-      return std::make_shared<TopNPlanNode>(optimized_plan->output_schema_, sort_plan.GetChildAt(0), order_bys, limit);
+  if(optimized_plan->GetType() == PlanType::Limit){
+    const auto &lim_plan = dynamic_cast<const LimitPlanNode &>(*optimized_plan);
+    if(lim_plan.GetChildPlan()->GetType()==PlanType::Sort){
+      const auto &sort_plan = dynamic_cast<const SortPlanNode &>(*lim_plan.GetChildPlan());
+      return std::make_shared<TopNPlanNode>(lim_plan.output_schema_,sort_plan.GetChildAt(0),sort_plan.GetOrderBy(),lim_plan.GetLimit());
     }
   }
-  return optimized_plan;
+  return plan;
 }
 }  // namespace bustub
